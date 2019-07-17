1120c977801bce6d584587c9cef2fb34f9a48e53
grekulanssi <anssi.grekula@helsinki.fi>
2019-07-17 14:25:57 +0300

    osa 1.3 kurssitiedot step3 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,28 +3,36 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
   const course = 'Half Stack application development!'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+  }
+  const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+  }
+  const part3 = {
+      name: 'State of a component',
+      exercises: 14
+  }
 
   return (
     <div>
         <Header c={course} />
         <div>
             <Content
-                p1={part1} ex1={exercises1}
-                p2={part2} ex2={exercises2}
-                p3={part3} ex3={exercises3} />
+                p1={part1.name} ex1={part1.exercises}
+                p2={part2.name} ex2={part2.exercises}
+                p3={part3.name} ex3={part3.exercises} />
         </div>
-        <Total number={exercises1 + exercises2 + exercises3} />
+        <Total number={part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
 }
 
 const Header = (props) => {
+    console.log('HEADER:')
+    console.log(props)
     return (
     <>
         <h1>{props.c}</h1>
@@ -33,6 +41,8 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+    console.log('CONTENT:')
+    console.log(props)
     return (
     <div>
         <Part p={props.p1} ex={props.ex1} />
@@ -43,6 +53,8 @@ const Content = (props) => {
   }
 
   const Part = (props) => {
+    console.log('PART:')
+    console.log(props)
       return (
           <>
           <p>{props.p} {props.ex}</p>
@@ -51,6 +63,8 @@ const Content = (props) => {
   }
 
 const Total = (props) => {
+    console.log('TOTAL:')
+    console.log(props)
     return (
     <>
         <p>Number of exercises {props.number}</p>
