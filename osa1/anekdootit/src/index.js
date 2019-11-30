547ed09bc677daaaba527f4a121ebf5545a1ea84
grekulanssi <anssi.grekula@helsinki.fi>
2019-11-30 18:11:14 +0200

    1.14*: anekdootit step3 done, could be cleaner/refactored, but at least it's functional now.

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -6,22 +6,33 @@ import './index.css';
 const App = (props) => {
     const [selected, setSelected] = useState(0)
     const [votes, setVotes] = useState(new Array(6).fill(0))
-//    console.log("anecdotes pituus: ", anecdotes.length)
-//    console.log("votes pituus: ", votes.length)
-//    console.log("voten sisältö: " + votes)
+    const [mostVotes, setMostVotes] = useState(0)
+    //console.log("anecdotes pituus: ", anecdotes.length)
+    //console.log("votes pituus: ", votes.length)
+    //console.log("voten sisältö: " + votes)
     return (
         <div className="main-container">
+            <h1>Anecdote of the day</h1>
             <div className="anecdote">
                 <p>"{props.anecdotes[selected]}"</p>
             </div>
             <p>has {votes[selected]} votes</p>
             <Button label='vote' onClick={() => {
-//                console.log("nyt klikattiin")
+                //console.log("nyt klikattiin")
                 const copy = [...votes]
                 copy[selected] += 1
+                // select the index whose value is highest:
+                setMostVotes(copy.indexOf(Math.max(...copy)))
                 setVotes(copy)
-            }  } />
+            }} />
             <Button label='next anecdote' onClick={() => setSelected(Math.floor(Math.random() * (anecdotes.length - 1 + 1)) + 0)} />
+            <div>
+                <h2>Anecdote with most votes</h2>
+                <div className="anecdote">
+                    <p>"{props.anecdotes[mostVotes]}"</p>
+                </div>
+                <p>has {votes[mostVotes]} votes</p>
+            </div>
         </div>
     )
 }
