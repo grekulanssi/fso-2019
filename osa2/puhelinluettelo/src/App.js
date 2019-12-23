1a10c70cd547ad84f9fdc5cc6cc3b02bc15295e7
grekulanssi <anssi.grekula@helsinki.fi>
2019-12-23 20:20:18 +0200

    2.9* puhelinluettelo step4 done with case insensitive filter

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,17 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567'
-    }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ newFiltertext, setNewFiltertext ] = useState('')
   
-
   const rows = () =>
-    persons.map(person =>
+    persons.filter(person => person.name.toLowerCase().includes(newFiltertext.toLowerCase())).map(person =>
       <li key={person.name}>{person.name} {person.number}</li>
     )
 
@@ -32,6 +32,8 @@ const App = () => {
     }
     setNewName('')
     setNewNumber('')
+    /*Filter is also wiped to let the user certainly see the name she just added*/
+    setNewFiltertext('')
   }
 
   const handleNameChange = (event) => {
@@ -44,26 +46,41 @@ const App = () => {
     setNewNumber(event.target.value)
     }
 
+  const handleFiltertextChange = (event) => {
+    console.log(event.target.value)
+    setNewFiltertext(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}>
-        <div>
-        name: <input 
-          value={newName}
-          onChange={handleNameChange}/><br />
-        number: <input 
-          value={newNumber}
-          onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <ul>
-        {rows()}
-      </ul>
+      <div>
+        filter shown with <input
+        value={newFiltertext}
+        onChange={handleFiltertextChange} />
+      </div>
+      <div>
+        <h2>add a new</h2>
+        <form onSubmit={addPerson}>
+          <div>
+            name: <input
+              value={newName}
+              onChange={handleNameChange} /><br />
+            number: <input
+              value={newNumber}
+              onChange={handleNumberChange} />
+          </div>
+          <div>
+            <button type="submit">add</button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <h2>Numbers</h2>
+        <ul>
+          {rows()}
+        </ul>
+      </div>
     </div>
   )
 
