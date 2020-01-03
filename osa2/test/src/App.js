76c46233370e5046d7f18ba744c7a5a402899527
grekulanssi <anssi.grekula@helsinki.fi>
2020-01-03 11:31:01 +0200

    test project updated following the course material

--- osa2/test/src/App.js
+++ osa2/test/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = (props) => {
     const [notes, setNotes] = useState([])
@@ -11,20 +11,40 @@ const App = (props) => {
 
     useEffect(() => {
         console.log('effect')
-        axios
-            .get('http://localhost:3001/notes')
-            .then(response => {
+        noteService
+            .getAll()
+            .then(initialNotes => {
                 console.log('promise fulfilled')
-                setNotes(response.data)
+                setNotes(initialNotes)
             })
     }, [])
     console.log('render', notes.length, 'notes')
+
+
+    const toggleImportanceOf = id => {
+        console.log('importance of ' + id + ' needs to be toggled')
+        const note = notes.find(n => n.id === id)
+        const changedNote = { ...note, important: !note.important }
+
+        noteService
+            .update(id, changedNote)
+            .then(returnedNote => {
+            setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+        })
+        .catch(error => {
+            alert(
+                `the note '${note.content}' was already deleted from server`
+            )
+            setNotes(notes.filter(n => n.id !== id))
+        })
+    }
 
     const rows = () =>
         notesToShow.map(note =>
             <Note
                 key={note.id}
                 note={note}
+                toggleImportance={() => toggleImportanceOf(note.id)}
             />
         )
 
@@ -36,11 +56,16 @@ const App = (props) => {
             content: newNote,
             date: new Date().toISOString(),
             important: Math.random() > 0.5,
-            id: notes.length + 1,
+            id: notes.length + 1
         }
 
-        setNotes(notes.concat(noteObject))
-        setNewNote('')
+        noteService
+        .create(noteObject)
+            .then(returnedNote => {
+                console.log(returnedNote)
+                setNotes(notes.concat(returnedNote))
+                setNewNote('')
+            })
     }
 
     const handleNoteChange = (event) => {
