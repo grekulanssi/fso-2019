aa50e094823aca33438f4b0edb5edd1f441844c6
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-15 17:36:24 +0300

    5.1 done, took 2 hours

--- osa5/bloglist-frontend/src/App.js
+++ osa5/bloglist-frontend/src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
+import './index.css'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,14 +18,71 @@ const App = () => {
     )
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password
+      })
+    setUser(user)
+    setUsername('')
+    setPassword('')
+    } catch (e) {
+      setErrorMessage('invalid credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000);
+    }
+  }
+
+  const loginForm = () => (
+    <div>
+      <h2>Please log in</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <span>username</span>
+          <input type="text" value={username} name="Username" onChange={
+            ({ target }) => setUsername(target.value)
+          }/>
+        </div>
+        <div>
+          <span>password</span>
+          <input type="password" value={password} name="Password" onChange={
+            ({ target }) => setPassword(target.value)
+          }/>
+        </div>
+        <button type="submit">log in</button>
+      </form>
+    </div>
+  )
+
+  const blogList = () => (
+    <div>
+        <h2>blogs</h2>
+        <ul>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+        </ul>
+      </div>
+  )
+
   return (
     <div>
-      <h2>blogs</h2>
-      <ul>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-      </ul>
+      <div>
+        <Notification message={errorMessage}/>
+        <h1>Blog app</h1>
+      </div>
+      <div>
+        {user === null ?
+          loginForm() :
+          <div>
+            <p>Welcome, {user.name}!</p>
+            {blogList()}
+          </div>
+          }
+      </div>
     </div>
   )
 }
--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const Blog = ({ blog }) => (
   <li>
-    {blog.title} {blog.author}
+    {blog.title} by {blog.author}
   </li>
 )
 
