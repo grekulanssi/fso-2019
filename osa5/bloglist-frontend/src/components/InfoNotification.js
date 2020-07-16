a43d6f58c1d8452eddd04dfd66e39364c965791f
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-16 14:56:00 +0300

    5.12 done, took 30 mins

--- osa5/bloglist-frontend/src/components/InfoNotification.js
+++ osa5/bloglist-frontend/src/components/InfoNotification.js
@@ -1,6 +1,6 @@
 import React from 'react'
 const InfoNotification = ({ message }) => (
-  <div className="infonotification">
+  <div className='infonotification'>
     <span>{message}</span>
   </div>
 )
