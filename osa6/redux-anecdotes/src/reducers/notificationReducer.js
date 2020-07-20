54edc85855372e0f3d5dc4e551d1a0302706ed0c
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-20 16:18:14 +0300

    6.10 done, took 15 min

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,20 @@
+const notificationAtStart = 'Nothing to notify!'
+
+const initialState = notificationAtStart
+
+const notificationReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'NOTIFY':
+      return action.data
+    default: return state
+  }
+}
+
+export const notify = (content) => {
+  return {
+    type: 'NOTIFY',
+    data: content
+  }
+}
+
+export default notificationReducer
