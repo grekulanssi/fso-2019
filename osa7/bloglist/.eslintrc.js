70522f705be50cfc74525ccb67ee1d5c76accf7b
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-24 14:09:54 +0300

    config files

---
+++ osa7/bloglist/.eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  "env": {
+      "browser": true,
+      "es6": true,
+      "jest/globals": true,
+      "cypress/globals": true
+  },
+  "extends": [ 
+      "eslint:recommended",
+      "plugin:react/recommended"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+      "ecmaFeatures": {
+          "jsx": true
+      },
+      "ecmaVersion": 2018,
+      "sourceType": "module"
+  },
+  "plugins": [
+      "react", "jest", "cypress"
+  ],
+  "rules": {
+      "indent": [
+          "error",
+          2, { "SwitchCase": 1 }
+      ],
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "quotes": [
+          "error",
+          "single"
+      ],
+      "semi": [
+          "error",
+          "never"
+      ],
+      "eqeqeq": "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": [
+          "error", "always"
+      ],
+      "arrow-spacing": [
+          "error", { "before": true, "after": true }
+      ],
+      "no-console": 0,
+      "react/prop-types": 0,
+      "max-len": [1, 200],
+    "no-shadow": 0,
+    "no-plusplus": 0,
+    "import/no-unresolved": "off",
+    "react/jsx-filename-extension": "off",
+  },
+  "settings": {
+      "react": {
+          "version": "detect"
+      }
+  }
+}
