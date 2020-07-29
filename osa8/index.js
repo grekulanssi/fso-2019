9d221ac2c5226db19fa7d3087b3e5869b545d48d
grekulanssi <anssi.grekula@helsinki.fi>
2020-07-29 20:12:44 +0300

    8.3 done, took 50 mins

--- osa8/index.js
+++ osa8/index.js
@@ -93,10 +93,18 @@ const typeDefs = gql`
     genres: [String]!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    id: ID!
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book]!
+    allAuthors: [Author]!
   }
 `
 
@@ -104,7 +112,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: (root) => books.filter(b => b.author === root.name).length
   }
 }
 
